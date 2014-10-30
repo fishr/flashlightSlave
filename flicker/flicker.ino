e8cdbd41619430179ec5cc312137100f3528ab2e
Norman Cao <maplenormandy@gmail.com>
2014-10-30 01:20:51 -0400

    Use real-time and add blackouts
    Type 'b' in serial to see it blackout

--- flicker/flicker.ino
+++ flicker/flicker.ino
@@ -1,20 +1,36 @@
 int ledPin = 13;
 
+// Variables relevant to the battery
 int battery = 255;
+volatile boolean usingBattery = true;
 
-long frame = 0;
-
-unsigned char dipTable1[32];
+// These are lookup tables for flickers
+byte dipTable1[32];
 int dipFrame1 = -1;
-unsigned char dipTable2[256];
+byte dipTable2[256];
 int dipFrame2 = -1;
 
+// These are used to keep timing
+long frame = 0;
+unsigned long lastMilli;
+
+// These are lookup tables for blackout
+volatile int blackoutFrame = 256;
+byte boTable[256];
+
+void blackout()
+{
+  blackoutFrame = -random(20);
+}
 
 void setup()
 {
+  // TODO: Take this out
+  Serial.begin(9600);
   
   pinMode(ledPin, OUTPUT);
-  
+
+  // Set up the lookup tables for the flickers  
   for (int i = 0; i < 32; ++i)
   {
     if (i < 16)
@@ -31,51 +47,98 @@ void setup()
     else
       dipTable2[i] = 255-(256-i)*4;
   }
+  // Blackout flicker
+  for (int i = 0; i < 256; ++i)
+  {
+    if (i < 8)
+      boTable[i] = 255-i*16;
+    else if (i < 24)
+      boTable[i] = 255-(24-i)*4-64;
+    else if (i < 32)
+      boTable[i] = 255-64-(i-24)*23;
+    else if (i < 256-24)
+      boTable[i] = 0;
+    else
+      boTable[i] = 255-(256-i)*10;
+  }
+  
+  lastMilli = millis();
 }
 
 void loop()
 {
-  if (dipFrame1 < 0 && random(64) < 1)
+  if (millis() - lastMilli >= 15)
   {
-    dipFrame1 = 0;
-  }
-  if (dipFrame2 < 0 && random(256) < 1)
-  {
-    dipFrame1 = 0;
-  }
-
-  int brightness = (battery+255)/2;
-  if (dipFrame1 >= 0)
-  {
-    brightness *= dipTable1[dipFrame1];
-    brightness /= 255;
-    dipFrame1 += 1;
-
-    if (dipFrame1 >= 32)
-    {
-      dipFrame1 = -1;
-    }
-  }
-  if (dipFrame2 >= 0)
-  {
-    brightness *= dipTable2[dipFrame2];
-    brightness /= 255;
-    dipFrame2 += 1;
-
-    if (dipFrame2 >= 256)
-    {
-      dipFrame2 = -1;
-    }
-  }
-
-  analogWrite(ledPin, brightness);
-
-  ++frame;
-  if (frame % 120 == 0 && battery > 0)
-  {
-    battery -= 1;
-  }
+    // This stuff does the flickering
+    if (dipFrame1 < 0 && random(64) < 1)
+      dipFrame1 = 0;
+    if (dipFrame2 < 0 && random(256) < 1)
+      dipFrame2 = 0;
   
-  delay(15);
+    int brightness = battery;
+    if (dipFrame1 >= 0)
+    {
+      brightness *= dipTable1[dipFrame1];
+      brightness /= 255;
+      dipFrame1 += 1;
+  
+      if (dipFrame1 >= 32)
+      {
+        dipFrame1 = -1;
+      }
+    }
+    if (dipFrame2 >= 0)
+    {
+      brightness *= dipTable2[dipFrame2];
+      brightness /= 255;
+      dipFrame2 += 1;
+  
+      if (dipFrame2 >= 256)
+      {
+        dipFrame2 = -1;
+      }
+    }
+    
+    if (blackoutFrame < 256)
+    {
+      if (blackoutFrame >= 0)
+      {
+        brightness *= boTable[blackoutFrame];
+        brightness /= 255;
+      }
+      blackoutFrame += random(2);
+    }
+  
+    if (usingBattery)
+    {
+      analogWrite(ledPin, brightness);
+      if (frame % 120 == 0 && battery > 0)
+      {
+        battery -= 1;
+        if (battery % 50 == 0)
+        {
+          // TODO: Transmit the battery level to master
+        }
+      }
+    }
+    else
+    {
+      analogWrite(ledPin, 0);
+    }
+  
+    ++frame;
+    lastMilli += 15;
+  }
 }
 
+
+// TODO: Replace with actual receive byte
+void serialEvent()
+{
+  while (Serial.available())
+  {
+    char inChar = (char)Serial.read();
+    if (inChar == 'b')
+      blackout();
+  }
+}
